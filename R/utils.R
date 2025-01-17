01cdcb85ec056a7005c588e9ab8728bed12263ab
Gabriele Pittarello <pwt887@ku.dk>
2025-01-17 16:12:09 +0100

    neural networks implemented

--- R/utils.R
+++ R/utils.R
@@ -22,3 +22,33 @@ create_response_variable_survival <- function(nodes, time_to_event, delta, event
 
   return(out)
 }
+
+preprocess_minmax <- function(varData) {
+  X <- as.numeric(varData)
+  2 * (X - min(X)) / (max(X) - min(X)) - 1
+}
+
+
+preprocess_catdummy <- function(varData, prefix) {
+
+  X <- as.integer(varData)
+  n0 <- length(unique(X))
+  n1 <- 2:n0
+  addCols <- purrr::map(n1, function(x, y) {as.integer(y == x)}, y = X) %>%
+    rlang::set_names(paste0(prefix, n1))
+  cbind(data, addCols)
+}
+
+poisson_nll <- function(y_true, y_pred, ...) {
+
+  # K        <- backend()
+  # browser()
+  # y_true   <- K$eval(y_true)
+  # y_pred   <- K$eval(y_pred)
+
+  out <- -sum(y_true * log(y_pred[2] * y_pred[1]) - y_pred[2] * y_pred[1])
+
+  return(out)
+
+}
+
